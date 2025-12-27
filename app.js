8b43806a73f71e0281e4dd321a9e7cdda3de5fa7
Daniel Onís Fabián <danielonisfabian@MacBook-Air-de-Daniel-244.local>
2025-12-27 17:57:32 +0100

    public directory

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 let app = express();
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
     res.send("Hello")
 })
 
 app.listen(8081, () => {
-    console.log("Server in 8s081")
+    console.log("Server in 8081")
 })
