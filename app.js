b8d743a6263c8ce345d570275b4183b504c24ba5
Daniel Onís Fabián <danielonisfabian@MacBook-Air-de-Daniel-244.local>
2025-12-27 17:44:59 +0100

    response /

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 let app = express();
 
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
 app.listen(8081, () => {
     console.log("Server in 8s081")
 })
